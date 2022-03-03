155f83cd5c056c6c1baec190178ed08c37617721
otavio-paz <pazotavio.n@gmail.com>
2022-03-03 16:15:22 -0300

    Add Aula04_Break_Continue examples

---
+++ Controlando_Fluxo_de_Execucao/Aula04_Break_Continue/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Aula04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Boolean condicao = true;
+            int valor;
+
+            do
+            {
+                Console.WriteLine("Digite um valor, 0 para sair");
+                valor = int.Parse(Console.ReadLine());
+
+                if(valor == 0)
+                {
+                    Console.WriteLine("Você saiu da aplicação.");
+                    break; //interrompera a aplicaçao
+                }
+                else
+                    Console.WriteLine($"O valor informado é: {valor}");
+                    continue; //irá continuar
+            }
+            while(condicao == true);
+        }
+    }
+}
