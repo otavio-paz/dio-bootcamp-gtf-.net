edff2cc5c99bd19fc1201a7c88133d6196f3f2ae
otavio-paz <pazotavio.n@gmail.com>
2022-03-03 16:07:06 -0300

    Add Aula03_Repeticoes examples

--- Controlando_Fluxo_de_Execucao/Aula03_Repeticoes/Program.cs
+++ Controlando_Fluxo_de_Execucao/Aula03_Repeticoes/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Aula03
+{
+    class Program
+    {
+        static void Main(string [] args)
+        {
+           int[] lista = {1,2,3,4,5};
+
+           foreach(int numero in lista)
+           {
+               Console.WriteLine(numero);
+           }
+        }
+    }
+}
+
+/*
+            int valor;
+           Console.WriteLine("Digite um valor:");
+           valor = int.Parse(Console.ReadLine());
+
+           for(int i = valor; i <= 10; i++)
+           {
+               Console.WriteLine(i);
+           } 
+*/
+
+
+
+
+
+/*
+            Boolean condicao = true;
+            int valor;
+
+            do
+            {
+                Console.WriteLine("Digite um valor, 0 para sair");
+                valor = int.Parse(Console.ReadLine());
+
+                if(valor == 0)
+                {
+                    Console.WriteLine("Você saiu da aplicação.");
+                    condicao = false;
+                }
+                else
+                    Console.WriteLine($"O valor informado é: {valor}");
+            }
+            while(condicao == true);
+*/
