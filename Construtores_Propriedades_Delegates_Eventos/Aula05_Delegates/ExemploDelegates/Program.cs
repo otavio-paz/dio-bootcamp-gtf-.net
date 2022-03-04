d2a72ea03147a35cf75f4197f1e8d13086ba5af5
otavio-paz <pazotavio.n@gmail.com>
2022-03-04 16:08:54 -0300

    Add Aula05_Delegates codes

---
+++ Construtores_Propriedades_Delegates_Eventos/Aula05_Delegates/ExemploDelegates/Program.cs
@@ -0,0 +1,21 @@
+﻿using static System.Console;
+using ExemploDelegates.models;
+
+namespace ExemploDelegates
+{
+    class Program
+    {
+        public delegate void Operacao(int x, int y); //declaracao do delegate com os parametros
+        //int x e int y são a assinatura, precisam condizem com o delegate na classe
+        static void Main(string[] args)
+        {
+                Operacao op = new Operacao(Calculadora.Somar); //passar o metodo somar pra delegate
+                op += Calculadora.Subtrair; //+= significa que estamos adicionando mais um metodo, sem perder a referencia do metodo anterior
+
+                //Operacao op2 = Calculadora.Somar; //outra forma de passar o metodo para o delegate
+
+                op.Invoke(10, 2); //invocar o delegate - primeira maneira
+                //op(10, 2); //chamar o delegate - segunda forma
+        }
+    }
+}
---
+++ Construtores_Propriedades_Delegates_Eventos/Aula05_Delegates/ExemploDelegates/models/Calculadora.cs
@@ -0,0 +1,15 @@
+namespace ExemploDelegates.models
+{
+    public class Calculadora
+    {
+        public static void Somar(int x, int y)
+        {
+            System.Console.WriteLine($"Adição: { x + y}");
+        }
+
+        public static void Subtrair(int x, int y)
+        {
+            System.Console.WriteLine($"Subtração: { x - y}");
+        }
+    }
+}
