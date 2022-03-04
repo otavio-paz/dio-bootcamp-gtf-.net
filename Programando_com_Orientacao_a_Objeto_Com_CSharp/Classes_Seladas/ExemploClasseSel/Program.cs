ece5e67f88b948071df5db41ed9ec2ef481f9362
otavio-paz <pazotavio.n@gmail.com>
2022-03-04 20:26:43 -0300

    Add Classe Object codes

--- Programando_com_Orientacao_a_Objeto_Com_CSharp/Classes_Seladas/ExemploClasseSel/Program.cs
+++ Programando_com_Orientacao_a_Objeto_Com_CSharp/Classes_Seladas/ExemploClasseSel/Program.cs
@@ -1,2 +1,14 @@
 ﻿using static System.Console;
 using ExemploClasseSel.models;
+
+namespace ExemploClasseSel 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Computador comp = new Computador();
+            System.Console.WriteLine(comp.ToString());
+        }
+    }
+}
---
+++ Programando_com_Orientacao_a_Objeto_Com_CSharp/Classes_Seladas/ExemploClasseSel/models/Computador.cs
@@ -0,0 +1,10 @@
+namespace ExemploClasseSel.models
+{
+    public class Computador
+    {
+        public override string ToString()
+        {
+            return "Sou uma classe computador";
+        }
+    }
+}
