b58fbce581a3ad4561e5c716f10b28c86f050f3d
otavio-paz <pazotavio.n@gmail.com>
2022-03-05 00:06:21 -0300

    Add Create File and Error example codes

---
+++ Trabalhando_com_Arquivos_e_Streams/SystemIO/Program.cs
@@ -0,0 +1,38 @@
+﻿using static System.Console;
+
+WriteLine("Digite o nome do arquivo: ");
+var nome = ReadLine();
+nome = LimparNome(nome);
+
+var path = Path.Combine(Environment.CurrentDirectory, $"{nome}.txt");
+
+CriarArquivo(path);
+
+WriteLine("Digite enter para finalizar... "); //para o terminal nao fechar
+ReadLine();
+
+static string LimparNome(string nome)
+{
+    foreach (var @char in Path.GetInvalidFileNameChars()) //verifica caracteres invalidos e substitui
+    {
+        nome = nome.Replace(@char, '-');
+    }
+    return nome;
+}
+static void CriarArquivo(string path)
+{
+    try
+    {
+        using var sw = File.CreateText(path);
+        sw.WriteLine("Está é a linha 1 do arquivo");
+        sw.WriteLine("Está é a linha 2 do arquivo");
+        sw.WriteLine("Está é a linha 3 do arquivo");
+    }
+    catch
+    {
+        WriteLine("O nome do arquivo está inválido.");
+    }
+
+}
+
+
