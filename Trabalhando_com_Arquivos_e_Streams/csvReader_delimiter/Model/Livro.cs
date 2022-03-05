fc9afcfd0a010f5a70a162deb544302c130ac4da
otavio-paz <pazotavio.n@gmail.com>
2022-03-05 17:47:41 -0300

    Add csvReader_delimiter examples

---
+++ Trabalhando_com_Arquivos_e_Streams/csvReader_delimiter/Model/Livro.cs
@@ -0,0 +1,13 @@
+namespace csvReader_delimiter.Model
+{
+    public class Livro
+    {
+        public string Titulo {get; set; }
+
+        public string Autor {get; set; }
+
+        public decimal Preco {get; set; }
+
+        //public DateOnly Lancamento {get; set; }
+    }
+}
