8d703ab32080dd80d99b8d2ff209539cc7fff8d5
otavio-paz <pazotavio.n@gmail.com>
2022-03-05 00:31:29 -0300

    Add File Move Copy codes

---
+++ Trabalhando_com_Arquivos_e_Streams/Directory_DirectoryInfo/Program.cs
@@ -0,0 +1,59 @@
+﻿using static System.Console;
+
+CriarDiretoriosGlobo();
+CriarArquivo();
+
+var origem = Path.Combine(Environment.CurrentDirectory, "brasil.txt");
+var destino = Path.Combine(Environment.CurrentDirectory, "globo", "América do Sul", "brasil.txt");
+
+static void MoverArquivo(string pathOrigem, string pathDestino)
+{
+    if(!File.Exists(pathOrigem))
+        WriteLine("Arquivo de origem não existe");
+    if(!File.Exists(pathDestino))
+        WriteLine("Arquivo já existe na pasta de destino.");
+
+    File.Move(pathDestino, pathDestino);
+}
+
+static void CriarArquivo()
+{
+    var path = Path.Combine(Environment.CurrentDirectory, "brasil.txt");
+
+    if (!File.Exists(path))
+    {
+        using var sw = File.CreateText(path);
+        sw.WriteLine("População: 213MM");
+        sw.WriteLine("IDH: 0,901");
+        sw.WriteLine("Dados atualizados em: 20/03/2022");
+    }
+
+
+}
+
+static void CriarDiretoriosGlobo()
+{
+    var path = Path.Combine(Environment.CurrentDirectory, "globo");
+
+    if (!File.Exists(path))
+    {
+        var dirGlobo = Directory.CreateDirectory(path);
+
+        var dirAmNorte = dirGlobo.CreateSubdirectory("América do Norte");
+        var dirAmCentral = dirGlobo.CreateSubdirectory("América Central");
+        var dirAmSul = dirGlobo.CreateSubdirectory("América do Sul");
+
+        dirAmNorte.CreateSubdirectory("EUA");
+        dirAmNorte.CreateSubdirectory("México");
+        dirAmNorte.CreateSubdirectory("Canadá");
+
+        dirAmCentral.CreateSubdirectory("Costa Rica");
+        dirAmCentral.CreateSubdirectory("Panamá");
+
+        dirAmSul.CreateSubdirectory("Brasil");
+        dirAmSul.CreateSubdirectory("Argentina");
+        dirAmSul.CreateSubdirectory("Paraguai");
+    }
+}
+
+
