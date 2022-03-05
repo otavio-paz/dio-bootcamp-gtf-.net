9860374e87da21c0a085d67970ef68d10143a92d
otavio-paz <pazotavio.n@gmail.com>
2022-03-05 14:52:45 -0300

    Add FileSystemWatcher examples

---
+++ Trabalhando_com_Arquivos_e_Streams/FileSystemWatcher/Program.cs
@@ -0,0 +1,33 @@
+﻿var path = @"C:\";
+using var fsw = new FileSystemWatcher(path);
+//como o FSW tem o IDisposable implementado, é recomendado usar o using 
+
+fsw.Created += OnCreated;
+fsw.Created += OnDeleted;
+fsw.Created += OnRenamed;
+
+fsw.EnableRaisingEvents = true; //para permitir q os eventos sejam disparados
+fsw.IncludeSubdirectories = true; //permitir para subdiretorios
+
+Console.WriteLine($"Monitorando eventos no caminho: {path}");
+Console.WriteLine("Pressione [enter] para finalizar...");
+Console.ReadLine();
+
+void OnCreated(object sender, FileSystemEventArgs e)
+{
+    Console.WriteLine($"Foi criado o arquivo {e.Name}");
+}
+
+
+
+void OnDeleted(object sender, FileSystemEventArgs e)
+{
+     Console.WriteLine($"Foi excluido o arquivo {e.Name}");
+}
+
+
+
+void OnRenamed(object sender, FileSystemEventArgs e)
+{
+     Console.WriteLine($"O arquivo {e.OldName} Foi renomeado o arquivo {e.Name}");
+}
