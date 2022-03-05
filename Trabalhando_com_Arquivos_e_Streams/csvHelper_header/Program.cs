00040a4efed61fa25a6cd3370ee16ea1e33cb2e9
otavio-paz <pazotavio.n@gmail.com>
2022-03-05 18:02:08 -0300

    Add csvHelper_header configuration examples

---
+++ Trabalhando_com_Arquivos_e_Streams/csvHelper_header/Program.cs
@@ -0,0 +1,40 @@
+﻿using static System.Console;
+using System.Globalization;
+using CsvHelper.Configuration;
+using CsvHelper;
+using csvHelper_header.Model;
+
+LerCsvComOutroDelimitador();
+
+WriteLine("Digite [enter] para fianlizar...");
+ReadLine();
+
+static void LerCsvComOutroDelimitador()
+{
+    var path = Path.Combine(Environment.CurrentDirectory,
+    "Entrada",
+    "Livros-preco-com-virgula.csv");
+
+    var fi = new FileInfo(path);
+    if (!fi.Exists)
+        throw new FileNotFoundException($"O arquivo {path} não existe.");
+
+    using var sr = new StreamReader(fi.FullName);
+    var csvConfig = new CsvConfiguration(CultureInfo.InvariantCulture)
+    {
+        Delimiter = ";"
+    };
+
+    using var csvReader = new CsvReader(sr, csvConfig); //implementa IDisposable, logo, using
+
+    var registros = csvReader.GetRecords<Livro>().ToList(); //GetRecords vai ler as linhas do arquivo
+
+    foreach (var registro in registros)
+    {
+        WriteLine($"Título: {registro.Titulo}");
+        WriteLine($"Preço: {registro.Preco}");
+        WriteLine($"Autor(a): {registro.Autor}");
+        WriteLine($"Data de Lançamento: {registro.Lancamento}");
+        WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+    }
+}
