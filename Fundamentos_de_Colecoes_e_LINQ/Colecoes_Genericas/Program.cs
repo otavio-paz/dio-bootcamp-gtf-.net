8863dc6ba22ee346a4814009af1dcaeba7791d5c
otavio-paz <pazotavio.n@gmail.com>
2022-03-06 19:24:12 -0300

    Add Colecoes_Genericas codes

---
+++ Fundamentos_de_Colecoes_e_LINQ/Colecoes_Genericas/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Colecoes_Genericas.helper;
+
+OperacoesLista opLista = new OperacoesLista(); //instanciando classe
+List<string> estados = new List<string>{"SP", "MG", "BA"}; //declaraçao da lista -> nao precisa explicitar a capacidade
+string[] estadosArray = new string[2]{"SC", "RS"};
+
+System.Console.WriteLine($"Quantidade de elementos na lista: {estados.Count}");
+
+opLista.ImprimirListaString(estados);
+
+estados.Insert(1, "RJ"); //RJ passa para o indice 1 e o restante vai pra frente do indice
+
+opLista.ImprimirListaString(estados);
+
+
+
+//estados.AddRange(estadosArray);
