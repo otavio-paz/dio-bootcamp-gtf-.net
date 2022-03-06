54575a76b10223d7e4651c47d7571f920b0faa5a
otavio-paz <pazotavio.n@gmail.com>
2022-03-06 16:51:56 -0300

    Add BubbleSort code

---
+++ Fundamentos_de_Colecoes_e_LINQ/Ordenando_Bubble_Sort/Program.cs
@@ -0,0 +1,12 @@
+﻿using Ordenando_Bubble_Sort.helper;
+
+OperacoesArray op = new OperacoesArray();
+int[] array = new int[5]{6, 3, 8, 1, 9};
+
+System.Console.WriteLine("Array original: ");
+op.ImprimirArray(array);
+
+op.OrdenarBubblesort(array);
+
+System.Console.WriteLine("Array ordenado:");
+op.ImprimirArray(array);
---
+++ Fundamentos_de_Colecoes_e_LINQ/Ordenando_Bubble_Sort/helper/OperacoesArray.cs
@@ -0,0 +1,34 @@
+namespace Ordenando_Bubble_Sort.helper
+{
+    public class OperacoesArray
+    {
+
+        public void OrdenarBubblesort(int[] array)
+        {
+            int temp = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length - 1; j++)
+                {
+                    {
+                        if (array[j] > array[j + 1])
+                        {
+                            temp = array[j + 1];
+                            array[j + 1] = array[j];
+                            array[j] = temp;
+                        }
+                    }
+                }
+            }
+        }
+
+        public void ImprimirArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                System.Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
+
