f7eb82a7341c5adabd095bb036c73d7f321636b5
otavio-paz <pazotavio.n@gmail.com>
2022-03-05 22:12:48 -0300

    Add Aula02_Exceptions codes

---
+++ Tratamento_de_Erro_Com_Exceptions/Aula02/Program.cs
@@ -0,0 +1,27 @@
+﻿int a = 100;
+int b = 0;
+
+try
+{
+    var resultado = Dividir(a, b);
+    System.Console.WriteLine($"O valor da divisão de {a} por {b} é igual a {resultado}");
+}
+catch (DivideByZeroException ex) when (a < 0) //mais especifica
+{
+    System.Console.WriteLine(ex.Message);
+}
+catch (Exception ex) //mais generalizada
+{
+    System.Console.WriteLine(ex.Message);
+}
+finally
+{
+    System.Console.WriteLine("Finalmente a divisão foi finalizada");
+}
+
+static double Dividir(int x, int y)
+{
+    if (y == 0)
+        throw new ArithmeticException();
+    return x / y;
+}
