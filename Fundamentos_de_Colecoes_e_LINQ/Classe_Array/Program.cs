aac222f672733e1a451c861c28b960d9578ec351
otavio-paz <pazotavio.n@gmail.com>
2022-03-06 17:31:08 -0300

    Update Classe_Array codes

---
+++ Fundamentos_de_Colecoes_e_LINQ/Classe_Array/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Classe_Array.helper;
+
+OperacoesArray op = new OperacoesArray();
+
+int[] array = new int[5] { 6, 3, 8, 1, 9 };
+
+System.Console.WriteLine("Array original: ");
+op.ImprimirArray(array);
+
+op.Ordenar(ref array); //usando a classe array
+
+System.Console.WriteLine("Array ordenado:");
+op.ImprimirArray(array);
---
+++ Fundamentos_de_Colecoes_e_LINQ/Classe_Array/helper/OperacoesArray.cs
@@ -0,0 +1,36 @@
+using System;
+namespace Classe_Array.helper
+{
+    public class OperacoesArray
+    {
+
+        public void OrdenarBubbleSort(ref int[] array)
+        {
+            int temp = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        temp = array[j + 1];
+                        array[j + 1] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+        }
+        public void ImprimirArray(int[] array)
+        {
+            var linha = string.Join(", ", array);
+            System.Console.WriteLine(linha);
+        }
+        public void Ordenar(ref int[] array)
+        {
+            Array.Sort(array); //usando classe array, metodo sort
+        }
+                            
+    }
+}
+
