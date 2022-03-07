980a7b2a89323674a1fa3a1ad1199e119b7f86ac
otavio-paz <pazotavio.n@gmail.com>
2022-03-07 20:39:58 -0300

    Add Desafio_03 code

---
+++ Desafios_Basicos_GFT_Start_4/Desafio_03/Program.cs
@@ -0,0 +1,17 @@
+﻿//RESULTADO - 3/3
+using System; 
+using System.Collections.Generic; 
+using System.Linq;
+
+class MinhaClasse {
+       static void Main()
+        {
+            var totalDeCasosDeTeste = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < totalDeCasosDeTeste; i++)
+            {
+               Console.WriteLine(string.Join(" ",
+               Console.ReadLine().Split(" ").ToList().Distinct().OrderBy(item => item)));
+            }
+      }
+}
