856e36463cc23e7073ab57ea72b9febbf9961b09
otavio-paz <pazotavio.n@gmail.com>
2022-03-06 22:40:23 -0300

    Add first ASP.NET project

---
+++ Desenvolvimento_de_Aplicacoes_DotNet/CursoMVC_DIO/CursoMVC_DIO/Models/Categoria.cs
@@ -0,0 +1,10 @@
+﻿namespace CursoMVC_DIO.Models
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+
+
+    }
+}
---
+++ Desenvolvimento_de_Aplicacoes_DotNet/CursoMVC_DIO/CursoMVC_DIO/Models/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CursoMVC_DIO.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; } //falando q tem uma tabela categorias
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database-Cursomvc_dio;Integrated Security-True");
+        }
+    }
+}
