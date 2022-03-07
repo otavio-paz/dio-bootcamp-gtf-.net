900c7d0da9c5f08d46c1b67bfb6257711fc9fba0
otavio-paz <pazotavio.n@gmail.com>
2022-03-07 20:25:26 -0300

    Add Desafio_02 code

---
+++ Desafios_Basicos_GFT_Start_4/Desafio_02/Program.cs
@@ -0,0 +1,52 @@
+﻿//RESULTADO 3/3
+
+using System;
+using System.Globalization;
+
+class minhaClasse
+{
+
+    public static bool VerificarTriangulo(double a, double b, double c)
+    {
+        double b_menos_c = Math.Abs(b - c); // | b - c | < a < b + c
+        double a_menos_c = Math.Abs(a - c); // | a - c | < b < a + c
+        double a_menos_b = Math.Abs(a - b); // | a - b | < c < a + b
+
+
+        if (b_menos_c < a && a < b + c && a_menos_c < a && a < b + c && a_menos_b < a && a < b + c)
+            return true; //é um triangulo
+        else
+            return false; //não é um triangulo
+    }
+
+    public static void CalcularPerimetroTriangulo(double a, double b, double c)
+    {
+        double perimetro = a + b + c;
+
+        System.Console.WriteLine($"Perimetro = {perimetro.ToString("N1")}");
+    }
+
+    public static void CalcularAreaTrapezio(double a, double b, double c)
+    {
+        double area = ((a + b) * c) / 2;
+        System.Console.WriteLine($"Area = {area.ToString("N1")}");
+    }
+    static void Main(string[] args)
+    {
+
+        double a, b, c;
+        string[] valor = Console.ReadLine().Split();
+        a = Convert.ToDouble(valor[0], CultureInfo.InvariantCulture);
+        b = Convert.ToDouble(valor[1], CultureInfo.InvariantCulture);
+        c = Convert.ToDouble(valor[2], CultureInfo.InvariantCulture);
+
+        //condição de existencia de triangulo
+        bool condTriangulo = VerificarTriangulo(a, b, c);
+        if (condTriangulo)
+            CalcularPerimetroTriangulo(a, b, c);
+        else
+            CalcularAreaTrapezio(a, b, c);
+
+    }
+
+}
